2f80d4b58abb57cb1192d9d146798bf1ad970e41
Luke Fitzpatrick <56657300+luke-fitz@users.noreply.github.com>
2025-05-04 07:53:38 +1000

    Make requested edits

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Playback speed factor
-let playbackSpeedFactor = 200;
+const playbackSpeedFactor = 200;
 
 // Precomputed styles
 const mediaQuery = window.matchMedia("screen and (max-width: 768px)");
@@ -19,18 +19,6 @@ const minLaneHeight = parseInt(
 // Reset beach vs ocean background widths upon resize
 window.addEventListener('resize', setArenaBackground);
 window.addEventListener('DOMContentLoaded', setArenaBackground);
-
-window.addEventListener('DOMContentLoaded', () => {
-  setArenaBackground();
-
-  const slider = document.getElementById('playback-speed-slider');
-  const speedValue = document.getElementById('playback-speed-value');
-
-  slider.addEventListener('input', () => {
-    playbackSpeedFactor = parseInt(slider.value);
-    speedValue.textContent = slider.value;
-  });
-});
 
 // Fetch the event data
 fetch('./data/event.json')
